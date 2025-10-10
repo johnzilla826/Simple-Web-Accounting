658ca26cb02740fbbefbc812333760f753f76308
johnzilla826 <229241950+johnzilla826@users.noreply.github.com>
2025-10-09 21:58:10 -0500

    edit accounts

--- src/routes/accounting.ts
+++ src/routes/accounting.ts
@@ -5,18 +5,29 @@ const router = Router();
 
 // View accounts
 router.get("/accounts/view", async (req, res) => {
-  const { rows } = await query("SELECT * from account");
-  res.json(rows);
+  try {
+    const { rows } = await query("SELECT * from account");
+    res.json(rows);
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500);
+  }
 });
 
 // View individual account
 router.get("/accounts/view/:number", async (req, res) => {
   const { number } = req.params;
-  const { rows } = await query(
-    "SELECT * FROM account WHERE account_number = $1",
-    [number]
-  );
-  res.send(rows);
+
+  try {
+    const { rows } = await query(
+      "SELECT * FROM account WHERE account_number = $1",
+      [number]
+    );
+    res.send(rows);
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500);
+  }
 });
 
 // Create account GET
@@ -28,20 +39,51 @@ router.get("/accounts/new/", async (req, res) => {
 router.post("/accounts/create", async (req, res) => {
   const { name, number, type } = req.body;
 
-  await query(
-    "INSERT INTO account (account_name, account_number, account_type) VALUES ($1, $2, $3)",
-    [name, number, type]
-  );
-  res.redirect("/accounts/view");
+  try {
+    await query(
+      "INSERT INTO account (account_name, account_number, account_type) VALUES ($1, $2, $3)",
+      [name, number, type]
+    );
+    res.redirect("/accounts/view");
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500);
+  }
 });
 
-router.get("/apple", (req, res) => {
-  const peopleColors = [
-    { name: "Jim", color: "red" },
-    { name: "Bob", color: "blue" },
-    { name: "Dan", color: "green" },
-  ];
-  res.render("apple", { peopleColors });
+// Edit account GET
+router.get("/accounts/edit/:number", async (req, res) => {
+  const { number } = req.params;
+
+  try {
+    const { rows } = await query(
+      "SELECT * FROM account WHERE account_number = $1",
+      [number]
+    );
+
+    const account = rows[0];
+
+    res.render("editAccountForm", { account });
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500);
+  }
+});
+
+// Edit account POST
+router.post("/accounts/edit", async (req, res) => {
+  const { id, name, number, type } = req.body;
+
+  try {
+    query(
+      "UPDATE account SET account_name = $1, account_number = $2, account_type = $3 WHERE account_id = $4",
+      [name, number, type, id]
+    );
+    res.redirect("/accounts/view");
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500);
+  }
 });
 
 export { router };
